7cc8176f4589b6766e4dceddeb214f68e8738f53
Thomas Griesmayer <griesmayer@spengergasse.at>
2026-05-22 10:26:16 +0200

    Add home and price view. No cards!!!

--- src/main/java/at/spengergasse/views/home/HomeView.java
+++ src/main/java/at/spengergasse/views/home/HomeView.java
@@ -2,8 +2,10 @@ package at.spengergasse.views.home;
 
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
@@ -13,23 +15,56 @@ import org.vaadin.lineawesome.LineAwesomeIconUrl;
 @PageTitle("Home")
 @Route("")
 @Menu(order = 0, icon = LineAwesomeIconUrl.GLOBE_SOLID)
-public class HomeView extends HorizontalLayout {
+public class HomeView extends VerticalLayout {
 
     private TextField name;
     private Button sayHello;
 
     public HomeView() {
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
-        sayHello.addClickShortcut(Key.ENTER);
+        setSpacing(false);
 
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+        setAlignItems(Alignment.CENTER);
 
-        add(name, sayHello);
+        H1 company = new H1("Griesy Taxi");
+        company.getStyle()
+                .set("font-family", "cursive")
+                .set("font-size", "6rem")
+                .set("margin", "0");
+
+        H2 subName = new H2("... the best taxi in town ...");
+        subName.getStyle()
+                .set("margin", "0")
+                .set("color", "gray");
+
+        Image logo = new Image("images/logo.png", "Griesy Taxi Logo");
+        logo.setWidth("800px");
+
+        Paragraph line1 = new Paragraph("Griesy Taxi steht für zuverlässigen, komfortablen und pünktlichen Fahrservice. Unser Unternehmen bietet sichere und angenehme Fahrten für Privatpersonen, Geschäftsreisende und Touristen. Egal ob kurze Strecken innerhalb der Stadt oder längere Fahrten – bei Griesy Taxi stehen Kundenzufriedenheit, Freundlichkeit und Professionalität immer an erster Stelle.");
+        line1.setWidth("500px");
+        line1.getStyle()
+                .set("font-size", "22px")
+                .set("line-height", "1.6")
+                .set("text-align", "left");
+
+        Paragraph line2 = new Paragraph("Mit modernen Fahrzeugen und erfahrenen Fahrern sorgt Griesy Taxi für eine entspannte und stressfreie Mobilität. Wir legen großen Wert auf Sauberkeit, Sicherheit und einen erstklassigen Service. Unsere Kunden profitieren von flexiblen Fahrzeiten, fairen Preisen und einer unkomplizierten Buchung. ");
+        line2.setWidth("500px");
+        line2.getStyle()
+                .set("font-size", "22px")
+                .set("line-height", "1.6")
+                .set("text-align", "left");
+
+        Paragraph line3 = new Paragraph("Griesy Taxi ist mehr als nur ein Fahrdienst – wir sind Ihr verlässlicher Partner für jede Fahrt. Ob Flughafentransfer, Geschäftsreise, Krankenhausfahrt oder spontaner Transport: Wir bringen Sie sicher und bequem an Ihr Ziel. Vertrauen Sie auf Qualität, Erfahrung und persönlichen Service mit Griesy Taxi.");
+        line3.setWidth("500px");
+        line3.getStyle()
+                .set("font-size", "22px")
+                .set("line-height", "1.6")
+                .set("text-align", "left");
+
+        H3 name = new H3("Griesy Taxi GmbH");
+        H3 street = new H3("Spengergasse 20");
+        H3 city = new H3("1050 Wien");
+
+        add(company, subName, logo, line1, line2, line3, name, street, city);
     }
 
 }
--- src/main/java/at/spengergasse/views/prices/PricesView.java
+++ src/main/java/at/spengergasse/views/prices/PricesView.java
@@ -1,5 +1,6 @@
 package at.spengergasse.views.prices;
 
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
@@ -18,19 +19,53 @@ public class PricesView extends VerticalLayout {
     public PricesView() {
         setSpacing(false);
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
+        H1 company = new H1("Griesy Taxi");
+        company.getStyle()
+                .set("font-family", "cursive")
+                .set("font-size", "6rem")
+                .set("margin", "0");
 
-        H2 header = new H2("This place intentionally left empty");
-        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
-        add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+        H2 subName = new H2("... the best taxi in town ...");
+        subName.getStyle()
+                .set("margin", "0")
+                .set("color", "gray");
 
-        setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+        H2 pricelist = new H2("Price List");
+        pricelist.getStyle()
+                .set("margin", "0")
+                .set("color", "gray");
+
+        H2 district1 = new H2("1050 Wien");
+        Paragraph priceDay1 = new Paragraph("Preis KM Tag: 1.20€");
+        Paragraph priceNight1 = new Paragraph("Preis KM Nacht: 1.70€");
+        Paragraph priceEnter1 = new Paragraph("Zusätzlich kommen noch 3.00€ für die Bestellung dazu!");
+
+        H2 district2 = new H2("1030 Wien");
+        Paragraph priceDay2 = new Paragraph("Preis KM Tag: 1.40€");
+        Paragraph priceNight2 = new Paragraph("Preis KM Nacht: 1.90€");
+        Paragraph priceEnter2 = new Paragraph("Zusätzlich kommen noch 3.00€ für die Bestellung dazu!");
+
+        H2 district3 = new H2("1080 Wien");
+        Paragraph priceDay3 = new Paragraph("Preis KM Tag: 2.20€");
+        Paragraph priceNight3 = new Paragraph("Preis KM Nacht: 2.70€");
+        Paragraph priceEnter3 = new Paragraph("Zusätzlich kommen noch 2.00€ für die Bestellung dazu!");
+
+        H2 district4 = new H2("1010 Wien");
+        Paragraph priceDay4 = new Paragraph("Preis KM Tag: 2.90€");
+        Paragraph priceNight4 = new Paragraph("Preis KM Nacht: 4.00€");
+        Paragraph priceEnter4 = new Paragraph("Zusätzlich kommen noch 5.00€ für die Bestellung dazu!");
+
+        H2 district5 = new H2("Rest Wien");
+        Paragraph priceDay5 = new Paragraph("Preis KM Tag: 2.00€");
+        Paragraph priceNight5 = new Paragraph("Preis KM Nacht: 2.40€");
+        Paragraph priceEnter5 = new Paragraph("Zusätzlich kommen noch 3.00€ für die Bestellung dazu!");
+
+        add(company, subName, pricelist,
+                district1, priceDay1, priceNight1, priceEnter1,
+                district2, priceDay2, priceNight2, priceEnter2,
+                district3, priceDay3, priceNight3, priceEnter3,
+                district4, priceDay4, priceNight4, priceEnter4,
+                district5, priceDay5, priceNight5, priceEnter5);
     }
 
 }
